967a46cabbb17cca84e201b63e7a23553792b96e
Munaashe <denis.siduna@gmail.com>
2026-07-20 20:48:18 +0200

    initialise the UI

---
+++ user-interface/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-slate-100 px-6 py-10">
+      <header className="mx-auto w-full max-w-5xl">
+        <h1 className="">
+          here we go again
+        </h1>
+      </header>
+    </main>
+  )
+}
+
+export default App
